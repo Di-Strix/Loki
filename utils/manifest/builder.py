b50adf6efe7404aa3c8895585aec80f33dab807f
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2026-04-30 23:54:53 +0200

    update tts and vc output filenames

--- utils/manifest/builder.py
+++ utils/manifest/builder.py
@@ -2,13 +2,12 @@ from utils.manifest.types import TTSManifest, VCManifest, VGManifest
 from hashlib import sha256
 from pathlib import Path
 
+
 def build_tts_manifest(
-    text: str, language: str, target_emotion: str, tts_dir: Path, file_prefix: str = ""
+    text: str, language: str, target_emotion: str, tts_dir: Path
 ) -> TTSManifest:
     text_hash = sha256(text.encode()).hexdigest()[:5]
-    filename = (
-        f"{file_prefix + '.' if file_prefix else ''}{target_emotion}.{text_hash}.wav"
-    )
+    filename = f"{target_emotion}.{text_hash}.wav"
     output_path = tts_dir / filename
 
     return TTSManifest(
@@ -23,11 +22,16 @@ def build_vc_manifest(
     source_audio_path: Path,
     voice_reference_path: Path,
     vc_dir: Path,
-    file_prefix: str = "",
     source_audio_text: str | None = None,
-    voice_reference_text: str | None = None
+    voice_reference_text: str | None = None,
 ) -> VCManifest:
-    filename = f"{file_prefix + '_' if file_prefix else ''}from_{source_audio_path.stem}_to_{voice_reference_path.stem}.wav"
+    path_hash = sha256(str(source_audio_path).encode())
+    path_hash.update(str(voice_reference_path).encode())
+    path_hash = path_hash.hexdigest()[:5]
+
+    filename = (
+        f"{source_audio_path.stem}_to_{voice_reference_path.stem}.{path_hash}.wav"
+    )
     output_path = vc_dir / filename
     return VCManifest(
         source_audio_path=source_audio_path.resolve(),
@@ -39,11 +43,14 @@ def build_vc_manifest(
 
 
 def build_vg_manifest(
-    audio_path: Path, face_reference_path: Path, output_path: Path, target_emotion: str | None = None
+    audio_path: Path,
+    face_reference_path: Path,
+    output_path: Path,
+    target_emotion: str | None = None,
 ) -> VGManifest:
     return VGManifest(
         audio_path=audio_path.resolve(),
         face_reference_path=face_reference_path.resolve(),
         output_path=output_path.resolve(),
-        target_emotion=target_emotion
+        target_emotion=target_emotion,
     )
